6ac282d04376593e3a9d651a25e73acc48d3099b
LakeHP <lake.hupo.2001@outlook.com>
2023-06-12 18:23:24 +0800

    20230612

--- src/hello.svelte
+++ src/hello.svelte
@@ -3,39 +3,71 @@
     let xmlText;
     let xmltestend;
 
-    // 拉取 RSS 数据
-    fetch(RSS_URL)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`${response.status} ${response.statusText}`);
-            }
-            return response.text();
-        })
-        .then((data) => {
-            xmlText = data;
-            console.log(xmlText);
+    // // 拉取 RSS 数据
+    // fetch(RSS_URL)
+    //     .then((response) => {
+    //         if (!response.ok) {
+    //             throw new Error(`${response.status} ${response.statusText}`);
+    //         }
+    //         return response.text();
+    //     })
+    //     .then((data) => {
+    //         xmlText = data;
+    //         console.log(xmlText);
 
-            // 解析 XML
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(xmlText, "application/xml");
-            const entries = Array.from(
-                xmlDoc.getElementsByTagName("entry")
-            ).map((entry) => ({
-                title: entry.getElementsByTagName("title")[0].textContent,
-                published:
-                    entry.getElementsByTagName("published")[0].textContent,
-                summary: entry.getElementsByTagName("summary")[0].textContent,
-            }));
-            console.log(xmltestend = entries[0]["published"]);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+    //         // 解析 XML
+    //         const parser = new DOMParser();
+    //         const xmlDoc = parser.parseFromString(xmlText, "application/xml");
+    //         const entries = Array.from(
+    //             xmlDoc.getElementsByTagName("entry")
+    //         ).map((entry) => ({
+    //             title: entry.getElementsByTagName("title")[0].textContent,
+    //             published:
+    //                 entry.getElementsByTagName("published")[0].textContent,
+    //             summary: entry.getElementsByTagName("summary")[0].textContent,
+    //         }));
+    //         console.log(xmltestend = entries[0]["published"]);
+    //     })
+    //     .catch((error) => {
+    //         console.error(error);
+    //     });
+
+    import Parser from 'rss-parser';
+
+interface Item {
+  title: string;
+  link: string;
+  description: string;
+  pubDate: Date;
+}
+
+async function getRssItems(url: string): Promise<Item[]> {
+  const parser = new Parser();
+  const feed = await parser.parseURL(url);
+  return feed.items.map(item => ({
+    title: item.title,
+    link: item.link,
+    description: item.contentSnippet ?? item.description ?? '',
+    pubDate: new Date(item.pubDate),
+  }));
+}
+
+
+getRssItems(RSS_URL)
+  .then(items => {
+    console.log(items);
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+
+
 </script>
 
 <div id="hello">
-    {xmltestend} 
-      <!-- {xmltestend[0]['summary']}   {xmltestend[0]['title']}   
+    {xmltestend}
+    <!-- {xmltestend[0]['summary']}   {xmltestend[0]['title']}   
     {xmltestend[1]['published']}   {xmltestend[1]['summary']}   {xmltestend[1]['title']}   
     {xmltestend[2]['published']}   {xmltestend[2]['summary']}   {xmltestend[2]['title']}    -->
 </div>
